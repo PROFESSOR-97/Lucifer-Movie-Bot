0cc6fab106d61cb1b5817ed97963f3578a9d4fe5
Bikash Maity <120736828+PROFESSOR-97@users.noreply.github.com>
2022-12-16 22:09:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/MrProfessor-99/LuciferMoringStar-RoboT.git /LuciferMoringStar-RoboT
+git clone -b master https://github.com/PROFESSOR-97/Lucifer-Movie-Bot.git /Lucifer-Movie-Bot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting LuciferMoringStar RoboT..."
+echo "Starting Lucifer Movie Bot..."
 python3 bot.py
