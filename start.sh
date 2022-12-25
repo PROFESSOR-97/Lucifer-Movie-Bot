a364721cebac842b898fe82f62ed70b0a98bcf71
Bikash Maity <120736828+PROFESSOR-97@users.noreply.github.com>
2022-12-25 16:48:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/PROFESSOR-97/Lucifer-Movie-Bot.git /Lucifer-Movie-Bot
-echo "Installing Requirements..."
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/PROFESSOR-97/Lucifer-Movie-Bot.git /Lucifer-Movie-Bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone $UPSTREAM_REPO /Lucifer-Movie-Bot
+fi
+cd /Lucifer-Movie-Bot
 pip3 install -U -r requirements.txt
-echo "Starting Lucifer Movie Bot..."
+echo "Starting DQ-The-File-Donor...."
 python3 bot.py
